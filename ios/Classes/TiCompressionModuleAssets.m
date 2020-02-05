4d128e694d1fb12aafa36ce6ee36dbb45afec7d4
Chris Williams <chris.a.williams@gmail.com>
2020-02-05 11:01:57 -0500

    style(ios): fix formatting

--- ios/Classes/TiCompressionModuleAssets.m
+++ ios/Classes/TiCompressionModuleAssets.m
@@ -3,20 +3,18 @@
  */
 #import "TiCompressionModuleAssets.h"
 
-extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+extern NSData *filterDataInRange(NSData *thedata, NSRange range);
 
 @implementation TiCompressionModuleAssets
 
 - (NSData *)moduleAsset
 {
-  
 
   return nil;
 }
 
 - (NSData *)resolveModuleAsset:(NSString *)path
 {
-  
 
   return nil;
 }
